ae9b205a71baa5d1e7f54b32cf134e214dd8f661
Richard Darst <rkd@zgib.net>
2022-02-07 16:13:48 +0200

    prompt-log: make customizable, add colors

--- prompt-log.sh
+++ prompt-log.sh
@@ -1,9 +1,13 @@
+
+
 alias prompt-log="_prompt-log-enable && trap '_prompt-log-exec' DEBUG"
 function _prompt-log-enable () {
     PS1_SIMPLE=1
-    BASH_LOG=${1:-~/demos.out}
-    PROMPT_LOG_COLOR=$BLUE
-    echo "logging to $BASH_LOG"
+    PROMPT_LOG_FILE=${1:-~/demos.out}
+    PROMPT_LOG_COLOR=${PROMPT_LOG_COLOR:-$LIGHT_BLUE}$
+    PROMPT_LOG_COLOR_CMD=${PROMPT_LOG_COLOR_CMD:-$BLUE}$
+    export PS1="$PROMPT_LOG_COLOR\w$PS1_INFO\$$NO_COLOR $PROMPT_LOG_COLOR_CMD"
+    echo "logging to $PROMPT_LOG_FILE"
     trap '_prompt-log-exec' DEBUG
     #echo "On an older shell, you may need to run \"trap '_prompt_demo_log_command' DEBUG\""
 }
@@ -15,7 +19,28 @@ _prompt-log-exec () {
         [[ "$PROMPT_COMMAND" =~ "$BASH_COMMAND" ]] && return # don't cause a preexec for $PROMPT_COMMAND
         local this_command=`HISTTIMEFORMAT= history 1 | sed -e "s/^[ ]*[0-9]*[ ]*//"`;
         # Here you can add other logging hooks
-        echo "$this_command" >> "$BASH_LOG"
+        echo "$this_command" >> "$PROMPT_LOG_FILE"
         # end
-        echo -ne "\e[0m"
+        echo -ne "\e[0m" # Reset color to neutral
 }
+
+
+    BLACK="\[\e[0;30m\]"
+    BLUE="\[\e[0;34m\]"
+    GREEN="\[\e[0;32m\]"
+    CYAN="\[\e[0;36m\]"
+    RED="\[\e[0;31m\]"
+    PURPLE="\[\e[0;35m\]"
+    BROWN="\[\e[0;33m\]"
+    LIGHT_GRAY="\[\e[0;37m\]"
+    DARK_GRAY="\[\e[1;30m\]"
+    LIGHT_BLUE="\[\e[1;34m\]"
+    LIGHT_GREEN="\[\e[1;32m\]"
+    LIGHT_CYAN="\[\e[1;36m\]"
+    LIGHT_RED="\[\e[1;31m\]"
+    LIGHT_PURPLE="\[\e[1;35m\]"
+    YELLOW="\[\e[1;33m\]"
+    WHITE="\[\e[1;37m\]"
+    NO_COLOR="\[\e[0m\]"
+    BGCOLOR=$LIGHT_BLUE
+    COLOR=$NO_COLOR
